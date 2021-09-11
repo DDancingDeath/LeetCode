e0437f010b376baffcd32bad12fbb3259fe0ea93
DDancingDeath <32215423+DDancingDeath@users.noreply.github.com>
2021-09-11 15:34:11 +0530

    Create find-customers-with-positive-revenue-this-year.sql

---
+++ Google/Easy/find-customers-with-positive-revenue-this-year.sql
@@ -0,0 +1,4 @@
+#1821. Find Customers With Positive Revenue this Year
+#https://leetcode.com/problems/find-customers-with-positive-revenue-this-year/
+
+Select customer_id from Customers where year = 2021 and revenue > 0;
